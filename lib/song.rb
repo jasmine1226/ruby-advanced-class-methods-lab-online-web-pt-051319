8f998db3fdf279f3856855730d45ccfb56a61dc4
Jasmine Chen <jasmine1226@gmail.com>
2019-06-02 21:22:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,40 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.create
+    song.name = name
+    song
+  end
+
+  def self.find_by_name(name)
+  match = {}
+    @@all.collect do |song|
+      if song.name == name
+        return match = song
+      end
+    end
+  match == {} ? false : match
+  end
+
+  def self.find_or_create_by_name(name) #fix this
+    if self.find_by_name(name) == false
+      self.create_by_name(name)
+    else
+      self.find_by_name(name)
+    end
+  end
+
 end
